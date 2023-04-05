4f2b9fb1b6b6b6cac4739ecf60b0e273c0e7aaad
Jorge Clavijo <jym272@gmail.com>
2023-04-05 16:24:24 -0300

    stress testing and nats-box dply, orders working properly

---
+++ port-forward.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+declare deploy=nats
+declare -A ports=(["host"]=4222 ["container"]=4222)
+
+declare container_name
+
+find_container_using_deploy() {
+  # retrieving only the first container name
+  container_name=$(kubectl get pods -l app="$deploy" -o jsonpath="{.items[0].metadata.name}")
+}
+
+find_container_using_deploy
+kubectl port-forward "$container_name" "${ports[host]}:${ports[container]}"
