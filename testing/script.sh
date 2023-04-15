01fd11244a16afe6eba708602e0cca385c7e4e71
Jorge Clavijo <jym272@gmail.com>
2023-04-14 21:28:14 -0300

    logging in all apis

--- testing/script.sh
+++ testing/script.sh
@@ -10,7 +10,7 @@ if [ -z "$id_new_ticket" ]; then
     exit 1
 fi
 # timeout until ticket is created in order-api 'cause of listener of ticket-created event
-sleep 2
+sleep 0.2
 # create order
 id_new_order=$(curl -s -b cookie -k https://ticketing.dev/api/orders -X POST -H "Content-Type: application/json" -d '{"ticketId": "'$id_new_ticket'"}' | jq -r '.order.id')
 if [ -z "$id_new_order" ]; then
